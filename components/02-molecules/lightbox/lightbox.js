61e53db434d9079e3e0e22e5d6bd61d77f9257ac
Travis Weghofer <taw219@lehigh.edu>
2021-08-27 15:06:56 -0400

    Several misc changes and fixes, need to edit more global text stuff

--- components/02-molecules/lightbox/lightbox.js
+++ components/02-molecules/lightbox/lightbox.js
@@ -5,15 +5,23 @@
             var parent = $(this);
             parent.find('.lightbox-trigger button').click(function() {
                 parent.find('.lightbox').addClass('lightbox--visible');
+                $('body').css({'overflow':'hidden'});
+                $(document).bind('scroll',function () { 
+                     window.scrollTo(0,0); 
+                });
                 parent.find('.lightbox-close button').focus();
                 $(document).on('keyup', function(e) {
                     if (e.key == 'Escape') {
                         parent.find('.lightbox').removeClass('lightbox--visible');
+                        $(document).unbind('scroll'); 
+                        $('body').css({'overflow':'visible'});
                     }
                 });
             });
             parent.find('.lightbox-close button').click(function() {
                 parent.find('.lightbox').removeClass('lightbox--visible');
+                $(document).unbind('scroll'); 
+                $('body').css({'overflow':'visible'});
             });
         });
       },
